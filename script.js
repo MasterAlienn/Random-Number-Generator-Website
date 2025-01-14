a96a44d7b06a047e5000aab739c29c1f0948709b
MasterAlienn <ryannforme@gmail.com>
2025-01-13 22:03:23 -0500

    fixed input

--- script.js
+++ script.js
@@ -3,20 +3,12 @@ var in2;
 var out;
 
 function takeInput() {
-    in1 = document.getElementById("in1").innerHTML;
-    in2 = document.getElementById("in2").innerHTML;
+    in1 = parseInt(document.getElementById("in1").value);
+    in2 = parseInt(document.getElementById("in2").value);
 }
 function genNum() {
     out = Math.floor(Math.random() * (in2 - in1 + 1)) + in1;
 }
 function sendOutput() {
-    /* if (out == NaN) {
-        if (typeof(in1) != Number) {
-            out = "Field 1 is not a number";
-        }
-        else if (typeof(in2) != Number) {
-            out = "Field 2 is not a number";
-        }
-    } */
     document.getElementById("out").innerHTML = out;
 }
